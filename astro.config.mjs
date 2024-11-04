b044032f5d95010627930eab43b1c0276f7bbe05
Brandon Gutierrez <bran09123@gmail.com>
2024-11-03 23:01:16 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
-
+import remarkMath from 'remark-math'; 
+import rehypeMath from "rehype-mathjax"
+import { remarkModifiedTime } from './remark-modified-time.mjs';
 import mdx from '@astrojs/mdx';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), icon()]
+  integrations: [mdx({
+    remarkPlugins: [remarkMath, remarkModifiedTime],
+    rehypePlugins: [rehypeMath]
+  }), icon()]
 });
